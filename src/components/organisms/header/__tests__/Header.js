2a54e8ecffe7390ca82e3fc82ba0bb9f707120b5
Freud <alexandromunera@gmail.com>
2021-05-06 16:42:40 -0500

    Added react testing library

--- src/components/organisms/header/__tests__/Header.js
+++ src/components/organisms/header/__tests__/Header.js
@@ -1,13 +1,26 @@
+// import React from "react"
+// import renderer from "react-test-renderer"
+
+// import Header from "../Header"
+
+// describe("Header", () => {
+//   it("renders correctly", () => {
+//     const tree = renderer
+//       .create(<Header siteTitle="Default Starter" />)
+//       .toJSON()
+//     expect(tree).toMatchSnapshot()
+//   })
+// })
+
 import React from "react"
-import renderer from "react-test-renderer"
+import { render } from "@testing-library/react"
 
-import Header from "../Header"
+// You have to write data-testid
+const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>
 
-describe("Header", () => {
-  it("renders correctly", () => {
-    const tree = renderer
-      .create(<Header siteTitle="Default Starter" />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+test("Displays the correct title", () => {
+  const { getByTestId } = render(<Title />)
+  // Assertion
+  expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome!")
+  // --> Test will pass
 })
